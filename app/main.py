1c2a48ce024bcc90ffaccabae465d580856122d6
Jonathan Ross <jwross24@vt.edu>
2022-12-08 12:39:49 +0800

    respond to ping

--- app/main.py
+++ app/main.py
@@ -3,7 +3,14 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, _ = server_socket.accept() # wait for client
+
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
