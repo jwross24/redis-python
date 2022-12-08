c63066af672e7793d6c822e085c6f4120e9f933c
Jonathan Ross <jwross24@vt.edu>
2022-12-08 12:51:38 +0800

    --allow-empty

--- app/main.py
+++ app/main.py
@@ -3,14 +3,14 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, _ = server_socket.accept() # wait for client
+    client_connection, _ = server_socket.accept()
 
-    with conn:
+    with client_connection:
         while True:
-            data = conn.recv(1024)
+            data = client_connection.recv(1024)
             if not data:
                 break
-            conn.sendall(b"+PONG\r\n")
+            client_connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
